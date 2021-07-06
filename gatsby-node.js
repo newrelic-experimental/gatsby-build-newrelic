cd61d033a7fd6f96e987f74b691731791c0b88ef
Ruairi Douglas <ruairidouglas@gmail.com>
2021-07-06 22:54:42 +0100

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,465 @@
+"use strict";
+require('newrelic');
+const newrelicFormatter = require('@newrelic/winston-enricher');
+const NewrelicWinston = require('newrelic-winston');
+const NewrelicLogs = require('winston-to-newrelic-logs');
+const winston = require('winston');
+const logger = winston.createLogger({
+    transports: [
+        new NewrelicLogs({ licenseKey: process.env.NR_LICENCE, apiUrl: 'https://log-api.newrelic.com' }),
+        new NewrelicWinston(),
+    ],
+    format: winston.format.combine(
+      winston.format.label({serviceName: 'GatsbyWinston'}),
+      newrelicFormatter()
+    )
+});
+
+
+var _process$env$BENCHMAR;
+
+const {
+  performance
+} = require(`perf_hooks`);
+
+const {
+  sync: glob
+} = require(`fast-glob`);
+
+const nodeFetch = require(`node-fetch`);
+
+const uuidv4 = require(`uuid/v4`);
+
+const {
+  execSync
+} = require(`child_process`);
+
+const fs = require(`fs`);
+
+console.error = function(d) { //
+  logger.error(d, {logee: 'ruairi'});
+};
+console.log = function(d) { //
+  logger.log({
+    level: 'info',
+    message: d
+  });
+};
+console.warn = function(d) { //
+  logger.log({
+    level: 'warn',
+    message: d
+  });
+};
+console.info = function(d) { //
+  logger.log({
+    level: 'info',
+    message: d
+  });
+};
+// var capcon = require('capture-console');
+// let output = ''
+// capcon.startCapture(process.stderr, function (stderr) {
+//   logger.error(stderr);
+// });
+// var stderr = capcon.captureStderr(function scope(stderr) {
+//   // whatever is done in here has stderr captured,
+//   // the return value is a string containing stderr
+//   console.log
+// });
+
+// // console.log(`HELLLOOOOOO ${stderr}`);
+ 
+// var stdout = capcon.captureStdout(function scope() {
+//   // whatever is done in here has stdout captured,
+//   // the return value is a string containing stdout
+// });
+ 
+// var stdio = capcon.captureStdio(function scope() {
+//   // whatever is done in here has both stdout and stderr captured,
+//   // the return value is an object with 'stderr' and 'stdout' keys
+// });
+console.log('alone or in pairs,');
+console.warn('and over your neighbors dog?');
+console.info('Whats great for a snack,');
+console.error('And fits on your back?');
+console.error('Its log, log, log');
+const bootstrapTime = performance.now();
+const CI_NAME = process.env.CI_NAME;
+const BENCHMARK_REPORTING_URL = "https://metric-api.newrelic.com/metric/v1";
+
+let lastApi; // Current benchmark state, if any. If none then create one on next lifecycle.
+
+let benchMeta;
+let nextBuildType = (_process$env$BENCHMAR = process.env.BENCHMARK_BUILD_TYPE) !== null && _process$env$BENCHMAR !== void 0 ? _process$env$BENCHMAR : `initial`;
+
+function reportInfo(...args) {
+  ;
+  (lastApi ? lastApi.reporter : console).info(...args);
+}
+
+function reportError(...args) {
+  ;
+  (lastApi ? lastApi.reporter : console).error(...args);
+}
+
+function execToStr(cmd) {
+  var _execSync;
+
+  return String((_execSync = execSync(cmd, {
+    encoding: `utf8`
+  })) !== null && _execSync !== void 0 ? _execSync : ``).trim();
+}
+
+function execToInt(cmd) {
+  // `parseInt` can return `NaN` for unexpected args
+  // `Number` can return undefined for unexpected args
+  // `0 | x` (bitwise or) will always return 0 for unexpected args, or 32bit int
+  return execToStr(cmd) | 0;
+}
+
+class BenchMeta {
+  constructor() {
+    this.flushing = undefined; // Promise of flushing if that has started
+
+    this.flushed = false; // Completed flushing?
+
+    this.localTime = new Date().toISOString();
+    this.timestamps = {
+      // TODO: we should also have access to node's timing data and see how long it took before bootstrapping this script
+      bootstrapTime,
+      // Start of this file
+      instantiationTime: performance.now(),
+      // Instantiation time of this class
+      benchmarkStart: 0,
+      // Start of benchmark itself
+      preInit: 0,
+      // Gatsby onPreInit life cycle
+      preBootstrap: 0,
+      // Gatsby onPreBootstrap life cycle
+      preBuild: 0,
+      // Gatsby onPreBuild life cycle
+      postBuild: 0,
+      // Gatsby onPostBuild life cycle
+      benchmarkEnd: 0 // End of benchmark itself
+
+    };
+    this.started = false;
+  }
+
+  getMetadata() {
+    var _process$env$BENCHMAR2;
+
+    let siteId = ``;
+
+    try {
+      var _JSON$parse$siteId, _JSON$parse, _process$env$GATSBY_T, _process$env;
+
+      // The tags ought to be a json string, but we try/catch it just in case it's not, or not a valid json string
+      siteId = (_JSON$parse$siteId = (_JSON$parse = JSON.parse((_process$env$GATSBY_T = (_process$env = process.env) === null || _process$env === void 0 ? void 0 : _process$env.GATSBY_TELEMETRY_TAGS) !== null && _process$env$GATSBY_T !== void 0 ? _process$env$GATSBY_T : `{}`)) === null || _JSON$parse === void 0 ? void 0 : _JSON$parse.siteId) !== null && _JSON$parse$siteId !== void 0 ? _JSON$parse$siteId : ``; // Set by server
+    } catch (e) {
+      siteId = `error`;
+      reportInfo(`Suppressed an error trying to JSON.parse(GATSBY_TELEMETRY_TAGS): ${e}`);
+    }
+    /**
+     * If we are running in netlify, environment variables can be attached using the INCOMING_HOOK_BODY
+     * extract the configuration from there
+     */
+
+
+    let buildType = nextBuildType;
+    nextBuildType = (_process$env$BENCHMAR2 = process.env.BENCHMARK_BUILD_TYPE_NEXT) !== null && _process$env$BENCHMAR2 !== void 0 ? _process$env$BENCHMAR2 : `DATA_UPDATE`;
+    const incomingHookBodyEnv = process.env.INCOMING_HOOK_BODY;
+
+    if (CI_NAME === `netlify` && incomingHookBodyEnv) {
+      try {
+        const incomingHookBody = JSON.parse(incomingHookBodyEnv);
+        buildType = incomingHookBody && incomingHookBody.buildType;
+      } catch (e) {
+        reportInfo(`Suppressed an error trying to JSON.parse(INCOMING_HOOK_BODY): ${e}`);
+      }
+    }
+
+    return {
+      buildId: process.env.BENCHMARK_BUILD_ID,
+      branch: process.env.BENCHMARK_BRANCH,
+      siteId,
+      contentSource: process.env.BENCHMARK_CONTENT_SOURCE,
+      siteType: process.env.BENCHMARK_SITE_TYPE,
+      repoName: process.env.BENCHMARK_REPO_NAME,
+      buildType: buildType
+    };
+  }
+
+  getData() {
+    var _process$cwd;
+
+    // Get memory usage snapshot first (just in case)
+    const {
+      rss,
+      heapTotal,
+      heapUsed,
+      external
+    } = process.memoryUsage();
+
+    for (const key in this.timestamps) {
+      this.timestamps[key] = Math.floor(this.timestamps[key]);
+    } // For the time being, our target benchmarks are part of the main repo
+    // And we will want to know what version of the repo we're testing with
+    // This won't work as intended when running a site not in our repo (!)
+
+
+    const gitHash = execToStr(`git rev-parse HEAD`); // Git only supports UTC tz through env var, but the unix time stamp is UTC
+
+    const unixStamp = execToStr(`git show --quiet --date=unix --format="%cd"`);
+    const commitTime = new Date(parseInt(unixStamp, 10) * 1000).toISOString();
+    const nodejsVersion = process.version; // This assumes the benchmark is started explicitly from `node_modules/.bin/gatsby`, and not a global install
+    // (This is what `gatsby --version` does too, ultimately)
+
+    const gatsbyCliVersion = execToStr(`node_modules/.bin/gatsby --version`);
+
+    const gatsbyVersion = require(`gatsby/package.json`).version;
+
+    const sharpVersion = fs.existsSync(`node_modules/sharp/package.json`) ? require(`sharp/package.json`).version : `none`;
+
+    const webpackVersion = require(`webpack/package.json`).version;
+
+    const publicJsSize = glob(`public/*.js`).reduce((t, file) => t + fs.statSync(file).size, 0);
+    const jpgCount = execToInt(`find public .cache  -type f -iname "*.jpg" -or -iname "*.jpeg" | wc -l`);
+    const pngCount = execToInt(`find public .cache  -type f -iname "*.png" | wc -l`);
+    const gifCount = execToInt(`find public .cache  -type f -iname "*.gif" | wc -l`);
+    const otherCount = execToInt(`find public .cache  -type f -iname "*.bmp" -or -iname "*.tif" -or -iname "*.webp" -or -iname "*.svg" | wc -l`);
+    const benchmarkMetadata = this.getMetadata();
+
+
+    const attributes = {
+      sessionId: process.gatsbyTelemetrySessionId || uuidv4(),
+      gitHash,
+      commitTime,
+      ci: process.env.CI || false,
+      ciName: CI_NAME || `local`,
+      nodejs: nodejsVersion,
+      gatsby: gatsbyVersion,
+      gatsbyCli: gatsbyCliVersion,
+      sharp: sharpVersion,
+      webpack: webpackVersion,
+      ...benchmarkMetadata,
+    }
+
+    const buildtimes = {
+      ...attributes,
+      bootstrapTime: this.timestamps.bootstrapTime,
+      instantiationTime: this.timestamps.instantiationTime, // Instantiation time of this class
+      benchmarkStart: this.timestamps.benchmarkStart, // Start of benchmark itself
+      preInit: this.timestamps.preInit, // Gatsby onPreInit life cycle
+      preBootstrap: this.timestamps.preBootstrap, // Gatsby onPreBootstrap life cycle
+      preBuild: this.timestamps.preBuild, // Gatsby onPreBuild life cycle
+      postBuild: this.timestamps.postBuild, // Gatsby onPostBuild life cycle
+      benchmarkEnd: this.timestamps.benchmarkEnd, // End of benchmark itself
+    }
+
+    var timestamp = Date.now();
+
+    const timeelapsed = this.timestamps.benchmarkEnd - this.timestamps.benchmarkStart;
+
+    return [{ 
+      "metrics":[{ 
+         "name":"jsSize", 
+         "type":"gauge", 
+         "value": publicJsSize, 
+         "timestamp": timestamp, 
+         "attributes":attributes 
+         },
+         { 
+          "name":"pngs", 
+          "type":"gauge", 
+          "value": pngCount, 
+          "timestamp": timestamp, 
+          "attributes":attributes 
+          },
+          { 
+            "name":"jpgs", 
+            "type":"gauge", 
+            "value": jpgCount, 
+            "timestamp": timestamp, 
+            "attributes":attributes 
+          },
+          { 
+            "name":"otherImages", 
+            "type":"gauge", 
+            "value": otherCount, 
+            "timestamp": timestamp, 
+            "attributes":attributes 
+          },
+          { 
+            "name":"gifs", 
+            "type":"gauge", 
+            "value": gifCount, 
+            "timestamp": timestamp, 
+            "attributes":attributes 
+          },
+          { 
+            "name":"memory-rss", 
+            "type":"gauge", 
+            "value": rss !== null && rss !== void 0 ? rss : 0, 
+            "timestamp": timestamp, 
+            "attributes":attributes 
+          },
+          { 
+            "name":"memory-heapTotal", 
+            "type":"gauge", 
+            "value": heapTotal !== null && heapTotal !== void 0 ? heapTotal : 0, 
+            "timestamp": timestamp, 
+            "attributes":attributes 
+          },
+          { 
+            "name":"memory-heapUsed", 
+            "type":"gauge", 
+            "value": heapUsed !== null && heapUsed !== void 0 ? heapUsed : 0, 
+            "timestamp": timestamp, 
+            "attributes":attributes 
+          },
+          { 
+            "name":"memory-external", 
+            "type":"gauge", 
+            "value": external !== null && external !== void 0 ? external : 0, 
+            "timestamp": timestamp, 
+            "attributes":attributes 
+          },
+          { 
+            "name":"build-times", 
+            "type":"gauge", 
+            "value": timeelapsed, 
+            "timestamp": timestamp, 
+            "attributes": buildtimes 
+          }
+        ] 
+    }];
+  }
+
+  markStart() {
+    if (this.started) {
+      reportError(`gatsby-plugin-benchmark-reporting: `, new Error(`Error: Should not call markStart() more than once`));
+      process.exit(1);
+    }
+
+    this.timestamps.benchmarkStart = performance.now();
+    this.started = true;
+  }
+
+  markDataPoint(name) {
+    if (BENCHMARK_REPORTING_URL) {
+      if (!(name in this.timestamps)) {
+        reportError(`Attempted to record a timestamp with a name (\`${name}\`) that wasn't expected`);
+        process.exit(1);
+      }
+    }
+
+    this.timestamps[name] = performance.now();
+  }
+
+  async markEnd() {
+    if (!this.timestamps.benchmarkStart) {
+      reportError(`gatsby-plugin-benchmark-reporting:`, new Error(`Error: Should not call markEnd() before calling markStart()`));
+      process.exit(1);
+    }
+
+    this.timestamps.benchmarkEnd = performance.now();
+    return this.flush();
+  }
+
+  async flush() {
+    const data = this.getData();
+    const json = JSON.stringify(data, null, 2);
+
+    if (!BENCHMARK_REPORTING_URL) {
+      reportInfo(`Gathered data: ` + json);
+      reportInfo(`BENCHMARK_REPORTING_URL not set, not submitting data`);
+      this.flushed = true;
+      return this.flushing = Promise.resolve();
+    }
+
+    reportInfo(`Gathered data: ` + json);
+    reportInfo(`Flushing benchmark data to remote server...`);
+    let lastStatus = 0;
+    this.flushing = nodeFetch(`${BENCHMARK_REPORTING_URL}`, {
+      method: `POST`,
+      headers: {
+        "content-type": `application/json`,
+        "Api-Key": "NRII-KkwQR2CQ81QCdfxTZOLc7G79RStiqG7R"
+      },
+      body: json
+    }).then(res => {
+      lastStatus = res.status;
+
+      if ([401, 500].includes(lastStatus)) {
+        reportInfo(`Got ${lastStatus} response, waiting for text`);
+        res.text().then(content => {
+          reportError(`Response error`, new Error(`Server responded with a ${lastStatus} error: ${content}`));
+          process.exit(1);
+        });
+      }
+
+      this.flushed = true; // Note: res.text returns a promise
+
+      return res.text();
+    });
+    this.flushing.then(text => reportInfo(`Server response: ${lastStatus}: ${text}`));
+    return this.flushing;
+  }
+
+}
+
+function init(lifecycle) {
+  if (!benchMeta) {
+    benchMeta = new BenchMeta(); // This should be set in the gatsby-config of the site when enabling this plugin
+
+    reportInfo(`gatsby-plugin-benchmark-reporting: Will post benchmark data to: ${BENCHMARK_REPORTING_URL || `the CLI`}`);
+    benchMeta.markStart();
+  }
+}
+
+process.on(`exit`, () => {
+  if (benchMeta && !benchMeta.flushed && BENCHMARK_REPORTING_URL) {
+    // This is probably already a non-zero exit as otherwise node should wait for the last promise to complete
+    reportError(`gatsby-plugin-benchmark-reporting error`, new Error(`This is process.exit(); Benchmark plugin has not completely flushed yet`));
+    process.exit(1);
+  }
+});
+
+async function onPreInit(api) {
+  lastApi = api;
+  init(`preInit`);
+  benchMeta.markDataPoint(`preInit`);
+}
+
+async function onPreBootstrap(api) {
+  lastApi = api;
+  init(`preBootstrap`);
+  benchMeta.markDataPoint(`preBootstrap`);
+}
+
+async function onPreBuild(api) {
+  lastApi = api;
+  init(`preBuild`);
+  benchMeta.markDataPoint(`preBuild`);
+}
+
+async function onPostBuild(api) {
+  if (!benchMeta) {
+    // Ignore. Don't start measuring on this event.
+    return;
+  }
+
+  lastApi = api;
+  benchMeta.markDataPoint(`postBuild`);
+  await benchMeta.markEnd();
+  benchMeta = undefined;
+}
+
+module.exports = {
+  onPreInit,
+  onPreBootstrap,
+  onPreBuild,
+  onPostBuild
+};
---
+++ zipkin-local.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault');
+
+exports.__esModule = true;
+exports.stop = exports.create = void 0;
+
+const _zipkin = _interopRequireDefault(require('zipkin'));
+
+const _zipkinTransportHttp = require('zipkin-transport-newrelic');
+
+const _zipkinJavascriptOpentracing = _interopRequireDefault(
+  require('zipkin-javascript-opentracing')
+);
+
+const _nodeFetch = _interopRequireDefault(require('node-fetch'));
+let logger;
+let recorder;
+/**
+ * Create and return an open-tracing compatible tracer. See
+ * https://github.com/opentracing/opentracing-javascript/blob/master/src/tracer.ts
+ */
+
+const create = () => {
+  logger = new _zipkinTransportHttp.HttpLogger({
+    // endpoint of local docker zipkin instance
+    endpoint: `https://trace-api.newrelic.com/trace/v1`,
+    headers: {
+      'Api-Key': process.env.NR_KEY,
+      'Data-Format': 'zipkin',
+      'Data-Format-Version': 2,
+    },
+  });
+  recorder = new _zipkin.BatchRecorder({
+    logger,
+    // timeout = 60 hours, must be longer than site's build time
+    timeout: 60 * 60 * 60 * 1000000,
+  });
+  // console.log(recorder)
+  const tracer = new _zipkinJavascriptOpentracing.default({
+    localServiceName: `gatsbyTest`,
+    serviceName: `gatsbyTest`,
+    // Sample 1 out of 1 spans (100%). When tracing production
+    // services, it is normal to sample 1 out of 10 requests so that
+    // tracing information doesn't impact site performance. But Gatsby
+    // is a build tool and only has "1" request (the
+    // build). Therefore, we must set this to 100% so that spans
+    // aren't missing
+    sampler: new _zipkin.sampler.CountingSampler(1),
+    traceId128Bit: true,
+    recorder,
+    kind: `client`,
+  });
+  return tracer;
+}; // Workaround for issue in Zipkin HTTP Logger where Spans are not
+// cleared off their processing queue before the node.js process
+// exits. Code is mostly the same as the zipkin processQueue
+// implementation.
+
+exports.create = create;
+
+const _processQueue = async () => {
+  if (logger.queue.length > 0) {
+    
+    const formattedQueue = logger.queue.map((trace) => {
+        const formatTrace = JSON.parse(trace)
+        formatTrace.localEndpoint = {}
+        formatTrace.localEndpoint.serviceName = formatTrace.annotations[0].endpoint.serviceName
+        delete formatTrace['annotations']
+        return JSON.stringify(formatTrace)
+    })
+    // console.log(typeof logger.queue[0])
+    
+    const postBody = `[${formattedQueue.join(',')}]`
+    try {
+      const response = await (0, _nodeFetch.default)(logger.endpoint, {
+        method: `POST`,
+        body: postBody,
+        headers: {
+          'Content-Type': 'application/json',
+          'Api-Key': process.env.NR_KEY,
+          'Data-Format': 'zipkin',
+          'Data-Format-Version': 2,
+        },
+      });
+
+      if (response.status !== 202) {
+        const err =
+          `Unexpected response while sending Zipkin data, status:` +
+          `${response.status}, body: ${postBody}`;
+        if (logger.errorListenerSet) logger.emit(`error`, new Error(err));
+        else console.error(err);
+      }
+    } catch (error) {
+      const err = `Error sending Zipkin data ${error}`;
+      // if (logger.errorListenerSet) logger.emit(`error`, new Error(err));
+      console.error(err);
+    }
+  }
+};
+/**
+ * Run any tracer cleanup required before the node.js process
+ * exits. For Zipkin HTTP, we must manually process any spans still on
+ * the queue
+ */
+
+const stop = async () => {
+  // First, write all partial spans to the http logger
+  // recorder.partialSpans.forEach((span, id) => {
+  //   console.log(recorder)
+  //   if (recorder._timedOut(span)) {
+  //     recorder._writeSpan(id);
+  //   }
+  // }); // Then tell http logger to process all spans in its queue
+
+  await _processQueue();
+};
+
+exports.stop = stop;
+// # sourceMappingURL=zipkin-local.js.map
