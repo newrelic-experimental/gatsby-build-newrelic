af4eaf11812a0b72476f19bc8133b6781a6d8ac7
Ruairi Douglas <ruairidouglas@gmail.com>
2021-07-28 21:42:32 +0100

    refactoring constants so that zipkin can access it

--- constants.js
+++ constants.js
@@ -1,28 +1,6 @@
   // You can use string format or `process.env` if you'd prefer
+  const uuidv4 = require(`uuid/v4`);
   module.exports = {
-    NR_KEY: process.env.NEW_RELIC_INSERT_KEY || '',
-    NR_LICENSE: process.env.NEW_RELIC_LICENSE_KEY || '',
-    SITE_NAME: process.env.SITE_NAME || '',
-    traces: {
-      collectTraces: true,
-      tags:{
-        'tracetest': 'gatsby',
-      }
-    },
-    logs: {
-      collectLogs: true,
-      tags: {
-        'logtest': 'gatsby',
-      }
-    },
-    metrics: {
-      collectMetrics: true,
-      tags: {
-        'metrictest': 'gatsby',
-      }
-    },
-    nrAgent: {
-      collectApm: true,
-    },
+    buildId: process.gatsbyTelemetrySessionId || uuidv4(),
   }
   
